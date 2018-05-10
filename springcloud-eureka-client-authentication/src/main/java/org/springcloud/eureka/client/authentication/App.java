1930a5612a471014bd875bb53af0b0a83b22302c
chixue <334548450@qq.com>
2018-05-10 15:00:40 +0800

    提交

---
+++ springcloud-eureka-client-authentication/src/main/java/org/springcloud/eureka/client/authentication/App.java
@@ -0,0 +1,13 @@
+package org.springcloud.eureka.client.authentication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
