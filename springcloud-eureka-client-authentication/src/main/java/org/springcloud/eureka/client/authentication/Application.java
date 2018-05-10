352b7f35d761acc8e4eb719e021106d2bc3c33b6
chixue <334548450@qq.com>
2018-05-10 15:34:42 +0800

    提交使用Eureka服务器进行身份验证示例

---
+++ springcloud-eureka-client-authentication/src/main/java/org/springcloud/eureka/client/authentication/Application.java
@@ -0,0 +1,27 @@
+package org.springcloud.eureka.client.authentication;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
